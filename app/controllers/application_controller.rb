3268b354cc148cabc35104a3f8b883c1cf106318
TrevorZylks <trevorzylks@gmail.com>
2021-10-12 21:04:51 -0700

    +2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
     include ActionController::Cookies
 
     def hello_world
-      session[:count] = (session[:count] || 0) + 1
+      session[:count] = (session[:count] || 0) + 2
       render json: { count: session[:count] }
     end
 end
