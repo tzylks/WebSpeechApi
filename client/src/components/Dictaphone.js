ce6d3ae8f29b1196cfabd55bf1b7cf08a8fac884
TrevorZylks <trevorzylks@gmail.com>
2021-10-14 15:29:19 -0700

    dialog

--- client/src/components/Dictaphone.js
+++ client/src/components/Dictaphone.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Dialog from '@mui/material/Dialog'
+import DialogContent from '@mui/material/DialogContent'
+import DialogActions from '@mui/material/DialogActions';
 
 const appId = 'c5e1f482-5caf-4444-a437-cfda656d5a36';
 const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
@@ -11,6 +14,23 @@ SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
 
 const Dictaphone = () => {
     const [colored, setColored] = useState('white')
+    const [questions, setQuestions] = useState([
+        'Where do you see yourself in five years?',
+        'What is a React Hook?',
+        'Describe an instance where you overcame adversity.',
+        'How would you loop through an array in Javascript to create a new array?',
+        'What is a linked list?',
+        'How do you see yourself contributing to our work culture?',
+        'How would you reverse a string?',
+        'What is a palindrome string?',
+        'How would you calculate the number of vowels and consonants in a string?'
+    ])
+
+    const [open, setOpen] = useState(true);
+    
+    const handleClose = () => {
+        setOpen(!open);
+    };
 
     const {
         transcript,
@@ -30,36 +50,43 @@ const Dictaphone = () => {
         'AH'
     ]
 
-    const questions = [
-        'Where do you see yourself in five years?',
-        'What is a React Hook?',
-        'Describe an instance where you overcame adversity.',
-        'How would you loop through an array in Javascript to create a new array?',
-        'What is a linked list?',
-        'How do you see yourself contributing to our work culture?',
-        'How would you reverse a string?',
-        'What is a palindrome string?',
-        'How to calculate the number of vowels and consonants in a string?'
-    ]
+    let question = '';
 
     function randomQuestion() {
-        return questions[Math.floor(Math.random()*questions.length)]
+        question = questions[Math.floor(Math.random() * questions.length)]
     }
 
-        let tryAgain = () => {
-            if (transcript.split(' ').some(i => badWords.includes(i))) {
-                return (
-                    <h1>TRY AGAIN</h1>
-                )
-            }
+    randomQuestion();
+
+    let tryAgain = () => {
+        if (transcript.split(' ').some(i => badWords.includes(i))) {
+            
+            return (
+                
+                <h1 style={{textAlign: 'center', fontSize: '140px'}}>TRY AGAIN</h1>
+            )
         }
+    }
 
     return (
         <>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                keepMounted
+            >
+                <DialogContent>
+                    {question}
+                </DialogContent>
+
+                <DialogActions>
+                    <Button onClick={handleClose}>close</Button>
+                </DialogActions>
+
+            </Dialog>
             <div style={{ backgroundColor: colored, textAlign: 'center' }}>
                 <Typography variant='h1'>Interview Trainer</Typography>
                 <Typography variant='h3'>Rules<Typography variant="h5">Respond to the prompt without using 'like', 'um', 'uh', or 'ah'</Typography></Typography>
-                <h4>{randomQuestion()}</h4>
                 <p>Microphone: {listening ? 'on' : 'off'}</p>
                 <Button
                     variant="outlined"
